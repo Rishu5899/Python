5ea522353d3f5fe8cf62a45ded19aa02622449e0
Rishu5899 <rajputrishu204@gmail.com>
2025-05-23 23:08:49 +0530

    multiple

---
+++ multiple.py
@@ -0,0 +1,12 @@
+x = "Dhruvi"
+y = "Fenil"
+z = "Urvi"
+
+print(type(x))
+print(y)
+print(z)
+
+x=y=z="Dhruvi"
+print(x)
+print(y)
+print(z)
---
+++ while.py
@@ -0,0 +1,11 @@
+
+j = int(input("Enter any Number :"))
+i = 1
+
+
+
+while i<=10:
+    value = j * i
+    #result = i * j
+    print(j,"*",i," = ",value)
+    i = i+1
