ac8b0f20d4ef1cddc6fa12f05cb164e5e0d34872
Rishu5899 <rajputrishu204@gmail.com>
2025-05-26 14:32:35 +0530

    input

---
+++ input.py
@@ -0,0 +1,10 @@
+Watch = ''' \nThis is my first program.
+            \nFor loop is used for writing all the character in line
+            \nLoop is necessary for the text 
+'''
+for character in Watch:
+    print(character)
+
+Name = "Rishu"
+print(len(Name))
+print(Name[-4: len(Name) - 2])
--- monday.py
+++ monday.py
@@ -1,3 +1,20 @@
 x = "9"
 y = "23.32"
 print (int(x)+ float(y))
+
+string = "23"
+number =  5
+string_rishu = int(string)
+sum = number + string_rishu
+print("The sum ")
+
+v = 3
+print(type(v))
+f = 4.6
+print(type(f))
+c = v + f
+print(c)
+print(type(c))
+
+name = "RISHU"
+print(name[0])
