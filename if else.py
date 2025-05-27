a8f9c9a641ba1a8f9eb4d0873a16fd8a51d306cb
Rishu5899 <rajputrishu204@gmail.com>
2025-05-27 10:25:19 +0530

    if else

---
+++ if else.py
@@ -0,0 +1,31 @@
+a = int(input("Enter the age"))
+print("Your age is",a)
+print(a<18)
+print(a<=18)
+print(a>=18)
+print(a==18)
+if (a>=18):
+    print("You are not allowed")
+else:
+    print("You are child")
+
+fees = int(input("Enter your fees"))
+budget = 50000
+if (fees <= budget):
+    print("The collage is selected")
+else:
+    print("Collage is Expensive")
+
+#Write a program of elif
+
+fees = int(input("Enter your Collage Fees"))
+examfees = int(input("Enter your Examfee"))
+
+if (fees + examfees < 50000):
+    print("Collage is Expensive")
+
+elif (fees + examfees > 80000):
+    print("Collage is Selected")
+
+else:
+    print("Collage is not budgetfriendly")
--- input.py
+++ input.py
@@ -7,4 +7,27 @@ for character in Watch:
 
 Name = "Rishu"
 print(len(Name))
-print(Name[-4: len(Name) - 2])
+print(Name[-4:-2])
+
+Collage = "Gls ***** uhhg  rkkg"
+print(Collage.upper())
+print(Collage.lower())
+print(Collage.rstrip("*"))
+print(Collage.replace("Gls","Sok"))
+print(Collage.split(" "))
+blogHeading = "introduction t0 jS"
+print(blogHeading.capitalize())
+print(len(blogHeading))
+print(len(blogHeading.center(100)))
+print(Collage.count("*"))
+print(Collage.find("lpp"))
+print(Collage.isalnum())
+print(Collage.isalpha())
+print(Collage.isspace())
+print(Collage.istitle())
+print(Collage.swapcase())
+print(Collage.title())
+
+
+
+
