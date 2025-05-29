281aa36944a81b24d8474aadb4a2d09db84382f5
Rishu5899 <rajputrishu204@gmail.com>
2025-05-29 15:06:44 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,17 @@
+no = int(input("Enter the Number"))
+for no in range(no,30,3):
+    print(no)
+
+n = int(input("Enter the Number"))
+for n in range(n):
+    print(n+1)
+
+b = int(input("Enter the number"))
+while (b>0):
+    print(b)
+    b = b - 1
+
+c = int(input("Enter the Number"))
+while (c<=1000):
+    print(c)
+    c = c + 1
