105a7160b2b72d05c826957932f3a5c9bd83aeb0
Rishu5899 <rajputrishu204@gmail.com>
2025-06-13 14:28:59 +0530

    for use in else

---
+++ for use in else .py
@@ -0,0 +1,18 @@
+e = int(input("Enter your number"))
+for a in  range(11):
+    print(e,"*",a,"=",a*e)
+    if e == 2:
+        break   
+else:
+    print("You didn't input the integer")
+
+
+q  = int(input("Enter your number"))   
+while q<=20:
+    q = q * 2 
+    print(q)
+    if q == 20:
+        print("stop")
+        break
+else:
+    print("no number")
