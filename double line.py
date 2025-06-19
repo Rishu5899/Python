25245b84062002f4b24d30d6694b41bd38fba92e
Rishu5899 <rajputrishu204@gmail.com>
2025-06-19 15:33:42 +0530

    double line

--- double line.py
+++ double line.py
@@ -1,6 +1,40 @@
 # Write a code of double line
-# An array is a double line comment
+# An array is a double line comment# W.A.P to Access the string after the index value 1.
+
+d = ["Elephant","Sparrow","Rabbit"]
+print(d[2])
 help("keywords")
 #Set Datatype
 d = {'a','x','t'}
 print(d)
+
+# W.A.P to print String using three quotes
+
+a = '''Rishu, Palak, Sak,Lam'''
+print(a)
+
+#  W.A.P to access the 1st position character using index value.
+
+s = ["apple","banana","pineapple"]
+print(s[2][1:])
+
+# W.A.P to Access the string before the index value 5.
+
+r = ["Rose","Sunflower","Marigold","Lilly","Lotus","Bluebell"]
+print (r[0:5])
+
+# W.A.P to Access the String between the index value 1 to 4.
+
+i = ["Root Bear","Pina Colada","Pepsi","Peach Butter","Peach"]
+print(i[1:4])
+
+#  W.A.P to print the string from the last index value.
+
+u = ["January","February","March","April","May"]
+# for c in u:
+print(u[-1])
+
+# W.A.P to print the String alternate character after the index value 1.
+
+x = ["Eternal","BSE","WIPRO","TCS","ADANI GREEN","RELIANCE"]
+print(x[2:6:1])
