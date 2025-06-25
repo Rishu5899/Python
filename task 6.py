63a8df75fe0e83a980f33d2a84c6b6b5d333114e
Rishu5899 <rajputrishu204@gmail.com>
2025-06-25 23:37:15 +0530

    day 3

---
+++ task 6.py
@@ -0,0 +1,12 @@
+# Write a program to find whether a given number(accept from the user) is even or odd and print the message to the user accordingly
+no = int(input("Enter the number"))
+if no%2==0:
+    print("Even Number")
+else:
+    print("Odd Number")
+# Write a program to find whether a given number(accept from the user) is greator then or less then and print the message to the user accordingly.
+s = int(input("Enter the number"))
+if s>0:
+    print("Greator Number")
+else:
+    print("Less Number")
