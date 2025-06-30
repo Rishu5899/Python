c9e5757859c76f467c23d0e973855da3fcf91569
Rishu5899 <rajputrishu204@gmail.com>
2025-06-30 15:44:17 +0530

    import.py

---
+++ import.py
@@ -0,0 +1,29 @@
+# import operator
+# abc = operator.add(5,8)
+# print(abc)
+# import math
+# we = math.sqrt (64)
+# print(we)
+# from math import sqrt, pi
+# sa = sqrt(81)*pi
+# print(sa)
+# import math
+# print(dir(math))
+# print(math.asin,type(math.asin))
+# import math as ma
+# sa = ma.ceil(54.8)
+# wq = ma.floor(54.8)
+# print(sa)
+# print(wq)
+# This fuction is not supported in visual Studio
+# from call import GoodMorning
+# GoodMorning()
+# print(GoodMorning)
+# Python program to execute 
+# main directly 
+print ("Always executed")
+
+if __name__ == "__main__": 
+    print ("Executed when invoked directly")
+else: 
+    print ("Executed when imported")
