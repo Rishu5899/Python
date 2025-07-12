e8e90608debc02f682af2ae5b178a7340a3cd0c4
Rishu5899 <rajputrishu204@gmail.com>
2025-07-12 15:35:12 +0530

    map

---
+++ constructor.py
@@ -0,0 +1,28 @@
+# class Menu:
+#     use = "Isha is dentist"
+#     name = "Dhrumil" # Class attr
+#     def __init__(self,name,age,write,mobileno,use):
+#         self.name = name # Object attribuite > class attribute
+#         self.age = age
+#         self.write = write
+#         self.mobileno = mobileno 
+    
+
+
+# c = Menu("Rishu age is",21,"and mobile no is",9023696890,"Python Developer")
+
+# print(c.name,c.age,c.write,c.mobileno)
+
+
+for q in range(1,11):
+   print(q*q)
+
+total = 0 
+for w in range(1,101):
+   total += w
+print(total)
+
+total = 0
+for i in range(1, 101):
+    total += i
+print( total)
--- map.py
+++ map.py
@@ -1,6 +1,6 @@
-# s = int(input("Enter the number of the table"))
-# for a in range(11):
-#     print(s,  "*",a,"=",s*a )
+s = int(input("Enter the number of the table"))
+for a in range(11):
+    print(s,  "*",a,"=",s*a )
 # a = 12
 # print(a*a)
 # def cube(x):
@@ -21,8 +21,8 @@
 #     return numb>2
 # da = list(filter(Filter_Function,li))
 # print(da)
-from functools import reduce
-# List of numbers
-numbers = [1,5,6,14,25]
-sum = reduce(lambda x, y: x + y, numbers)
-print(sum)
+# from functools import reduce
+# # List of numbers
+# numbers = [1,5,6,14,25]
+# sum = reduce(lambda x, y: x + y, numbers)
+# print(sum)
