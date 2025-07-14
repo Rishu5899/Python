86ef5d5d70db49910f59d2709f82ef79cc55d999
Rishu5899 <rajputrishu204@gmail.com>
2025-07-14 15:42:27 +0530

    Add files via upload

--- constructor.py
+++ constructor.py
@@ -2,7 +2,7 @@
 #     use = "Isha is dentist"
 #     name = "Dhrumil" # Class attr
 #     def __init__(self,name,age,write,mobileno,use):
-#         self.name = name # Object attribuite > class attribute
+#         self.name = name #  Oject attr , Object attribuite > class attribute
 #         self.age = age
 #         self.write = write
 #         self.mobileno = mobileno 
@@ -14,15 +14,37 @@
 # print(c.name,c.age,c.write,c.mobileno)
 
 
-for q in range(1,11):
+for q in range(1,100,2):
    print(q*q)
 
-total = 0 
-for w in range(1,101):
-   total += w
-print(total)
+# total = 0 
+# for w in range(1,101):
+#    total += w
+# print(total)
 
 total = 0
 for i in range(1, 101):
     total += i
-print( total)
+print(total)
+
+class Tution:
+ def __init__(self,Name,Fees,Yearly):
+     self.Name = Name
+     self.Fees = Fees
+     self.Yearly = Yearly
+ def welcome(self):
+   print("Welcome all Students")
+
+ def get_Name(self):
+    return self.Name   
+Name=int(input("Enter the number "))
+Fees=int(input("Enter the number "))
+Yearly=Name+Fees
+
+  
+
+s = Tution(Name,Fees,Yearly)
+print(s.Name,s.Fees,s.Yearly)
+
+s.welcome()
+print(s.Name)
